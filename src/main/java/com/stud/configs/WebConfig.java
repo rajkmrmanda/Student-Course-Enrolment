2c40203fb6590576b2a10e0d08071f1440e98f46
rajkmrmanda <145082007+rajkmrmanda@users.noreply.github.com>
2023-12-13 14:57:58 -0600

    Add files via upload

---
+++ src/main/java/com/stud/configs/WebConfig.java
@@ -0,0 +1,17 @@
+package com.stud.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/student/index");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
+
+}
